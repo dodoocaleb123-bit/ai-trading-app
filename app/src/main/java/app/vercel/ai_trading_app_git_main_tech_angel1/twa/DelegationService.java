a73afcd44dd280f911b81ed4fd9791c01d589468
Caleb <dodoocaleb123@gmail.com>
2026-08-16 16:34:20 +0000

    Update main.py with clean Telegram alert formatting

---
+++ app/src/main/java/app/vercel/ai_trading_app_git_main_tech_angel1/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.vercel.ai_trading_app_git_main_tech_angel1.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
